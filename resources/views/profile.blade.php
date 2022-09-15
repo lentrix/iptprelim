34c952123ebc983f65f25fd54abc276262d91166
lentrix <hawkmanlentrix@gmail.com>
2022-09-15 20:51:35 +0800

    initial

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prelim Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+        <nav class="nav justify-content-center">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+            <a href="{{url('/about')}}" class="nav-link">About Us</a>
+            <a href="{{url('/profile')}}" class="nav-link">Company Profile</a>
+        </nav>
+        <h1>Company Profile</h1>
+
+        <div class="card shadow-sm mx-auto" style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Company Profile</div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deleniti cum, velit sint eveniet repudiandae esse repellat minus in quisquam nam, unde dolore incidunt dolorem magnam enim tempore dolor id.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad deleniti cum, velit sint eveniet repudiandae esse repellat minus in quisquam nam, unde dolore incidunt dolorem magnam enim tempore dolor id.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
